4595f5ec382a7a434b41ed45ddb7354e5cdc341a
sulaweyo <sledge.sulaweyo@gmail.com>
2014-09-08 10:02:23 +0200

    cleaned excessive logging

--- lib/puppet/provider/download/ruby.rb
+++ lib/puppet/provider/download/ruby.rb
@@ -7,23 +7,17 @@ Puppet::Type.type(:download).provide(:ruby) do
 
   def download
     begin
-      Puppet.debug("Download URI is #{resource[:uri]}")
-      Puppet.debug("Destination is #{resource[:dest]}")
       uri = URI(resource[:uri])
       http = Net::HTTP.new(uri.host, uri.port)
       request = Net::HTTP::Get.new(uri.request_uri)
-      Puppet.debug("Download host is #{uri.host}")
-      Puppet.debug("Download uri is #{uri.request_uri}")
       http = Net::HTTP.start(uri.host)
       resp = http.request_get(uri.request_uri)
-      Puppet.debug("Response code #{resp.code}")
       redirects = 0
       while resp.code == '301' || resp.code == '302' 
-        Puppet.debug("Redirect response is #{resp.header['location']}")
+        Puppet.debug("Redirect to #{resp.header['location']}")
         newUri = URI.parse(resp.header['location'])
         http = Net::HTTP.start(newUri.host)
         resp = http.request_get(newUri.request_uri)
-        Puppet.debug("Redirect response code #{resp.code}")
         redirects += 1
         if (redirects > 10)
           raise 'too many redirects'
