8433af2b74e8596f8bcf5be938523e8ae4f85c2d
Sledge Sulaweyo <sledge.sulaweyo@gmail.com>
2015-03-18 10:44:35 +0100

    Add option to use SSL Add option to use basic auth by setting user & password

--- lib/puppet/provider/download/ruby.rb
+++ lib/puppet/provider/download/ruby.rb
@@ -1,29 +1,44 @@
-# This type allows dowloads of files via plain ruby 
+# This type allows dowloads of files via plain ruby
 #
 require 'puppet'
 require 'net/http'
 require 'uri'
 
 Puppet::Type.type(:download).provide(:ruby) do
-
   def download
     success = false
+    access_denied = false
     for retries in 1..3
       begin
         Puppet.debug("HTTP download uri is ... '#{resource[:uri]}'")
-        s3url = URI(resource[:uri])
-        http = Net::HTTP.new(s3url.host, s3url.port)
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        resp = http.request_get(s3url.request_uri)
-        redirects = 0
-        while resp.code == '301' || resp.code == '302' 
-          Puppet.debug("Redirect to #{resp.header['location']}")
-          s3url = URI.parse(resp.header['location'])
-          http = Net::HTTP.start(s3url.host, s3url.port)
+        myURI = URI(resource[:uri])
+        ssl = resource[:use_ssl]
+          if !ssl and resource[:uri].start_with? 'https'
+            ssl = true
+          end
+        http = Net::HTTP.new(myURI.host, myURI.port)
+        if (ssl)
           http.use_ssl = true
           http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-          resp = http.request_get(s3url.request_uri)
+        end
+        req = Net::HTTP::Get.new(myURI.path)
+        if nil != resource[:user] and nil != resource[:pass]
+          req.basic_auth(resource[:user], resource[:pass])
+        end
+        resp = http.request(req)
+        redirects = 0
+        while resp.code == '301' || resp.code == '302'
+          Puppet.debug("Redirect to #{resp.header['location']}")
+          myURI = URI.parse(resp.header['location'])
+          if (ssl)
+            http.use_ssl = true
+            http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+          end
+          req = Net::HTTP::Get.new(myURI.path)
+          if nil != resource[:user] and nil != resource[:pass]
+            req.basic_auth(resource[:user], resource[:pass])
+          end
+          resp = http.request(req)
           redirects += 1
           if (redirects > 10)
             raise 'too many redirects'
@@ -32,7 +47,7 @@ Puppet::Type.type(:download).provide(:ruby) do
         if resp.code == '200'
           Puppet.debug('Starting download...')
           file = File.open(resource[:dest], "wb")
-          http.request_get(s3url.request_uri) do |response|
+          http.request(req) do |response|
             response.read_body do |segment|
               file.write(segment)
             end
@@ -41,6 +56,9 @@ Puppet::Type.type(:download).provide(:ruby) do
             http.finish
           end
           success = true
+        elsif resp.code == '403'
+          access_denied = true
+          Puppet.alert("Server returned code 403: '#{resp.message}'")
         end
       rescue IOError => eio
         Puppet.crit("IO Exception during http download -> \n#{eio.inspect}")
@@ -58,18 +76,18 @@ Puppet::Type.type(:download).provide(:ruby) do
       else
         Puppet.info("Download failed - try again...")
       end
+      if access_denied
+        break
+      end
     end
     return success
   end
-  
 
   def exists?
-    Puppet.debug('Exists was called for download')
     false
   end
 
   def destroy
-    Puppet.debug('Destroy was called for download')
     true
   end
 
--- lib/puppet/type/download.rb
+++ lib/puppet/type/download.rb
@@ -13,6 +13,14 @@ Puppet::Type.newtype(:download) do
             uri  => 'http://www.example.com/download/example.txt',
             dest => '/tmp/example.txt'
           }
+          
+          download { 'my sll and basic auth download':
+           uri  => 'https://www.example.com/download/example.txt',
+           dest => '/tmp/example.txt',
+           user => 'user',
+           pass => 'pass'
+          }
+               
   
   EOS
 
@@ -20,16 +28,30 @@ Puppet::Type.newtype(:download) do
     defaultvalues
     defaultto :present
   end
-  
+
   newparam(:name, :namevar => true) do
     desc "The name for the download."
   end
-  
+
   newparam(:uri) do
     desc "The uri of the file to download."
   end
-    
+
   newparam(:dest) do
     desc "The destination file. Make sure the path to this file exists!"
+  end
+
+  newparam(:use_ssl, :boolean => true, :parent => Puppet::Parameter::Boolean) do
+    desc "Use SSL for the transfer? If the URL starts with https this is set automatically."
+    defaultto :false
+    newvalues(:true, :false)
+  end
+
+  newparam(:user) do
+    desc "A user to use for basic authentication."
+  end
+
+  newparam(:pass) do
+    desc "A pass to use for basic authentication."
   end
 end
